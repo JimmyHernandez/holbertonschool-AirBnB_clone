4a39b5bbf9fde2dbe73b30eb8586424645830420
Enyel019 <[el_dominic01@hotmail.com]>
2023-03-14 19:43:33 -0400

    console

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
     command interpreter."""
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -10,6 +12,7 @@ class HBNBCommand(cmd.Cmd):
         that inherits from the cmd module."""
 
     prompt = '(hbnb)'
+    __classes = {'BaseModel'}
 
     def do_quit(self, line):
         """Quit command to exit the program."""
@@ -21,8 +24,59 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, line):
         """Print the string "EOF" and then exits the program\
-            :param line: The line of text that the user entered."""
+           :param line: The line of text that the user entered."""
         return True
+
+    def do_create(self, *args):
+        """Create a new instance of the class."""
+
+        if not args:
+            print('**class name missing**')
+        elif args not in HBNBCommand.__classes:
+            print('** class doesn\'t exist **')
+
+    def do_show(self, line):
+        """Print the value of the variable named by the argument
+        :param line: The line of text that the user entered."""
+
+        arg = line.split()
+        obj_dict = storage.all()
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(arg[0], arg[1]) not in obj_dict:
+            print("** no instance found **")
+        else:
+            print(obj_dict["{}.{}".format(arg[0], arg[1])])
+
+    def do_destroy(self, line):
+        """It destroys the current
+        room and all the items in it
+        :param line: The line of text that the user entered.
+        """
+        arg = line.split()
+        obj_dict = storage.all()
+
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(arg[0], arg[1]) not in obj_dict.keys():
+            print("** no instance found **")
+        else:
+            del obj_dict["{}.{}".format(arg[0], arg[1])]
+            storage.save()
+
+    def do_all(self, arg):
+        pass
+
+    def do_update(self, arg):
+        pass
 
 
 if __name__ == '__main__':
