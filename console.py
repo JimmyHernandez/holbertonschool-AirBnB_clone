9a701bf0f4d0c4cb6bd93f16d8b6166fd6510c1d
JimmyHernandez <jimoem24@gmail.com>
2023-03-15 10:32:50 -0400

    adding User class

--- console.py
+++ console.py
@@ -4,9 +4,13 @@
 
 import cmd
 from models.base_model import BaseModel
+from models.user import User
 from models import storage
 
-classes = {"BaseModel": BaseModel}
+classes = {
+    "BaseModel": BaseModel,
+    "User": User
+}
 
 
 class HBNBCommand(cmd.Cmd):
