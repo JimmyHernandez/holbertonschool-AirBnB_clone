76d4cfc148c7847d22c2ecbe62092f31d53f0b77
JimmyHernandez <jimoem24@gmail.com>
2023-03-15 11:12:12 -0400

    Add new classes

--- console.py
+++ console.py
@@ -3,13 +3,24 @@
     command interpreter."""
 
 import cmd
+from models import storage
 from models.base_model import BaseModel
 from models.user import User
-from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
 
 classes = {
     "BaseModel": BaseModel,
-    "User": User
+    "User": User,
+    "State": State,
+    "city": City,
+    "Amenity": Amenity,
+    "Place": Place,
+    "Review": Review
 }
 
 
