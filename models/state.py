48f1884913cea08522de78d0b3965ae2aba5a75f
JimmyHernandez <jimoem24@gmail.com>
2023-03-15 11:13:12 -0400

    add new clases

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""The `State` class inherits from the `BaseModel` class, and has a `name` attribute."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Creating a public class attribute called name."""
+
+    name = ""
