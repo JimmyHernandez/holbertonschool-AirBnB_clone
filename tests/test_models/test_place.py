5f6a28568be4b7509018ffb9794b169a62d379a0
Enyel019 <[el_dominic01@hotmail.com]>
2023-03-18 11:34:40 -0400

    new update

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -6,30 +6,6 @@ from models.city import City
 from models.place import Place
 
 
-class TestBaseModel(unittest.TestCase):
-    """Test instances and methods from BaseModel class."""
-
-    def setUp(self):
-        self.a = BaseModel()
-
-    def test_class_exists(self):
-        """Test if class exists."""
-        res = "<class 'models.base_model.BaseModel'>"
-        self.assertEqual(str(type(self.a)), res)
-
-
-class TestCity(unittest.TestCase):
-    """Test instances and methods from City class."""
-
-    def setUp(self):
-        self.a = City()
-
-    def test_class_exists(self):
-        """Test if class exists."""
-        res = "<class 'models.city.City'>"
-        self.assertEqual(str(type(self.a)), res)
-
-
 class TestPlace(unittest.TestCase):
     """Test instances and methods from Place class."""
 
